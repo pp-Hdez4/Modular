4854af69d2424070d9830df788260b256c8fb0ce
José de Jesús Rodríguez Hernández <123794221+pp-Hdez4@users.noreply.github.com>
2023-12-21 23:27:42 -0600

    Creacion de vistas "home" y "encuesta"

--- core/views.py
+++ core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+
+def home(request):
+    return render(request, "core/home.html")
+
+def encuesta(request):
+    return render(request, "core/encuesta.html")
